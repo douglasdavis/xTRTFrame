ff54254864b542ec13d2777c85fa00f1b93d981a
Douglas Davis <ddavis@cern.ch>
2017-10-04 00:17:41 -0400

    remove unneeded setup script

--- setup.sh
+++
@@ -1,6 +0,0 @@
-export xTRTFrame_SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-lsetup 'sft releases/LCG_87/yamlcpp/0.5.1'
-export YAMLCPP_ROOT=$SFT_HOME_yamlcpp
-
-alias cmake_xtrtframe='cd $xTRTFrame_SRC/../build; cmake -DYAMLCPP_ROOT=$YAMLCPP_ROOT ../xTRTFrame'
