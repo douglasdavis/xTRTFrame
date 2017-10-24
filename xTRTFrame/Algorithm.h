5603dc3d58d8f69b2606847d35e4dd9b5319858f
Douglas Davis <ddavis@cern.ch>
2017-10-24 15:11:50 -0400

    includes file

--- xTRTFrame/Algorithm.h
+++ xTRTFrame/Algorithm.h
@@ -24,32 +24,7 @@
 #include <functional>
 
 // ATLAS
-#include <EventLoop/Algorithm.h>
-#include <EventLoop/Worker.h>
-#include <AsgTools/AnaToolHandle.h>
-#include <AsgAnalysisInterfaces/IGoodRunsListSelectionTool.h>
-#include <AsgAnalysisInterfaces/IPileupReweightingTool.h>
-#include <TrigConfxAOD/xAODConfigTool.h>
-#include <TrigDecisionTool/TrigDecisionTool.h>
-#include <TriggerMatchingTool/MatchingTool.h>
-#include <PathResolver/PathResolver.h>
-#include <InDetTrackSelectionTool/InDetTrackSelectionTool.h>
-
-#include <xAODTracking/TrackParticle.h>
-#include <xAODEventInfo/EventInfo.h>
-#include <xAODCore/ShallowCopy.h>
-#include <xAODMuon/MuonContainer.h>
-#include <xAODMuon/MuonAuxContainer.h>
-#include <xAODEgamma/EgammaxAODHelpers.h>
-#include <xAODEgamma/ElectronContainer.h>
-#include <xAODEgamma/ElectronAuxContainer.h>
-#include <xAODTruth/TruthParticleAuxContainer.h>
-#include <xAODTruth/TruthVertex.h>
-#include <xAODTruth/xAODTruthHelpers.h>
-#include <xAODTracking/VertexContainer.h>
-#include <xAODTracking/TrackMeasurementValidationContainer.h>
-#include <xAODTracking/TrackStateValidationContainer.h>
-#include <xAODTracking/TrackParticlexAODHelpers.h>
+#include <xTRTFrame/AtlasIncludes.h>
 
 // xTRTFrame
 #include <xTRTFrame/Utils.h>
---
+++ xTRTFrame/AtlasIncludes.h
@@ -0,0 +1,31 @@
+#ifndef xTRTFrame_AtlasIncludes_h
+#define xTRTFrame_AtlasIncludes_h
+
+#include <xAODTracking/TrackParticle.h>
+#include <xAODEventInfo/EventInfo.h>
+#include <xAODCore/ShallowCopy.h>
+#include <xAODMuon/MuonContainer.h>
+#include <xAODMuon/MuonAuxContainer.h>
+#include <xAODEgamma/EgammaxAODHelpers.h>
+#include <xAODEgamma/ElectronContainer.h>
+#include <xAODEgamma/ElectronAuxContainer.h>
+#include <xAODTruth/TruthParticleAuxContainer.h>
+#include <xAODTruth/TruthVertex.h>
+#include <xAODTruth/xAODTruthHelpers.h>
+#include <xAODTracking/VertexContainer.h>
+#include <xAODTracking/TrackMeasurementValidationContainer.h>
+#include <xAODTracking/TrackStateValidationContainer.h>
+#include <xAODTracking/TrackParticlexAODHelpers.h>
+
+#include <EventLoop/Algorithm.h>
+#include <EventLoop/Worker.h>
+#include <AsgTools/AnaToolHandle.h>
+#include <AsgAnalysisInterfaces/IGoodRunsListSelectionTool.h>
+#include <AsgAnalysisInterfaces/IPileupReweightingTool.h>
+#include <TrigConfxAOD/xAODConfigTool.h>
+#include <TrigDecisionTool/TrigDecisionTool.h>
+#include <TriggerMatchingTool/MatchingTool.h>
+#include <PathResolver/PathResolver.h>
+#include <InDetTrackSelectionTool/InDetTrackSelectionTool.h>
+
+#endif
