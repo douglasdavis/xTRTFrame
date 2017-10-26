20acbda2d10beb187d5aa09578b2e5538f07e6e4
Douglas Davis <ddavis@cern.ch>
2017-10-26 17:32:44 -0400

    no static

--- xTRTFrame/Accessors.h
+++ xTRTFrame/Accessors.h
@@ -20,53 +20,53 @@ namespace xTRT {
   namespace Acc {
 
     // Track
-    static const SG::AuxElement::ConstAccessor<float> eProbabilityToT          {"eProbabilityToT"};
-    static const SG::AuxElement::ConstAccessor<float> eProbabilityHT           {"eProbabilityHT"};
-    static const SG::AuxElement::ConstAccessor<float> TRTTrackOccupancy        {"TRTTrackOccupancy"};
-    static const SG::AuxElement::ConstAccessor<float> ToT_dEdx_noHT_divByL     {"ToT_dEdx_noHT_divByL"};
-    static const SG::AuxElement::ConstAccessor<float> ToT_usedHits_noHT_divByL {"ToT_usedHits_noHT_divByL"};
+    const SG::AuxElement::ConstAccessor<float> eProbabilityToT          {"eProbabilityToT"};
+    const SG::AuxElement::ConstAccessor<float> eProbabilityHT           {"eProbabilityHT"};
+    const SG::AuxElement::ConstAccessor<float> TRTTrackOccupancy        {"TRTTrackOccupancy"};
+    const SG::AuxElement::ConstAccessor<float> ToT_dEdx_noHT_divByL     {"ToT_dEdx_noHT_divByL"};
+    const SG::AuxElement::ConstAccessor<float> ToT_usedHits_noHT_divByL {"ToT_usedHits_noHT_divByL"};
 
 
-    static const SG::AuxElement::ConstAccessor<unsigned char> numberOfTRTHits     {"numberOfTRTHits"};
-    static const SG::AuxElement::ConstAccessor<unsigned char> numberOfTRTOutliers {"numberOfTRTOutliers"};
-    static const SG::AuxElement::ConstAccessor<unsigned char> numberOfInnermostPixelLayerHits
+    const SG::AuxElement::ConstAccessor<unsigned char> numberOfTRTHits     {"numberOfTRTHits"};
+    const SG::AuxElement::ConstAccessor<unsigned char> numberOfTRTOutliers {"numberOfTRTOutliers"};
+    const SG::AuxElement::ConstAccessor<unsigned char> numberOfInnermostPixelLayerHits
     {"numberOfInnermostPixelLayerHits"};
-    static const SG::AuxElement::ConstAccessor<unsigned char> numberOfNextToInnermostPixelLayerHits
+    const SG::AuxElement::ConstAccessor<unsigned char> numberOfNextToInnermostPixelLayerHits
     {"numberOfNextToInnermostPixelLayerHits"};
-    static const SG::AuxElement::ConstAccessor<unsigned char> numberOfPixelHits {"numberOfPixelHits"};
-    static const SG::AuxElement::ConstAccessor<unsigned char> numberOfSCTHits   {"numberOfSCTHits"};
+    const SG::AuxElement::ConstAccessor<unsigned char> numberOfPixelHits {"numberOfPixelHits"};
+    const SG::AuxElement::ConstAccessor<unsigned char> numberOfSCTHits   {"numberOfSCTHits"};
 
-    static const SG::AuxElement::ConstAccessor<float> numberDoF  {"numberDoF"};
-    static const SG::AuxElement::ConstAccessor<float> chiSquared {"chiSquared"};
+    const SG::AuxElement::ConstAccessor<float> numberDoF  {"numberDoF"};
+    const SG::AuxElement::ConstAccessor<float> chiSquared {"chiSquared"};
 
-    static const SG::AuxElement::ConstAccessor<
+    const SG::AuxElement::ConstAccessor<
       ElementLink<xAOD::TruthParticleContainer>
       > truthParticleLink{"truthParticleLink"};
 
-    static const SG::AuxElement::ConstAccessor<
+    const SG::AuxElement::ConstAccessor<
       std::vector<ElementLink<xAOD::TrackStateValidationContainer>>
       > msosLink{"msosLink"};
 
     // Drift Circle
-    static const SG::AuxElement::ConstAccessor<uint>  bitPattern  {"bitPattern"};
-    static const SG::AuxElement::ConstAccessor<char>  gasType     {"gasType"};
-    static const SG::AuxElement::ConstAccessor<int>   bec         {"bec"};
-    static const SG::AuxElement::ConstAccessor<int>   layer       {"layer"};
-    static const SG::AuxElement::ConstAccessor<int>   strawlayer  {"strawlayer"};
-    static const SG::AuxElement::ConstAccessor<int>   strawnumber {"strawnumber"};
-    static const SG::AuxElement::ConstAccessor<float> drifttime   {"drifttime"};
-    static const SG::AuxElement::ConstAccessor<float> tot         {"tot"};
-    static const SG::AuxElement::ConstAccessor<float> T0          {"T0"};
+    const SG::AuxElement::ConstAccessor<uint>  bitPattern  {"bitPattern"};
+    const SG::AuxElement::ConstAccessor<char>  gasType     {"gasType"};
+    const SG::AuxElement::ConstAccessor<int>   bec         {"bec"};
+    const SG::AuxElement::ConstAccessor<int>   layer       {"layer"};
+    const SG::AuxElement::ConstAccessor<int>   strawlayer  {"strawlayer"};
+    const SG::AuxElement::ConstAccessor<int>   strawnumber {"strawnumber"};
+    const SG::AuxElement::ConstAccessor<float> drifttime   {"drifttime"};
+    const SG::AuxElement::ConstAccessor<float> tot         {"tot"};
+    const SG::AuxElement::ConstAccessor<float> T0          {"T0"};
 
     // MSOS
-    static const SG::AuxElement::ConstAccessor<int>   type       {"type"};
-    static const SG::AuxElement::ConstAccessor<float> localX     {"localX"};
-    static const SG::AuxElement::ConstAccessor<float> localY     {"localY"};
-    static const SG::AuxElement::ConstAccessor<float> localTheta {"localTheta"};
-    static const SG::AuxElement::ConstAccessor<float> localPhi   {"localPhi"};
-    static const SG::AuxElement::ConstAccessor<float> HitZ       {"HitZ"};
-    static const SG::AuxElement::ConstAccessor<float> HitR       {"HitR"};
-    static const SG::AuxElement::ConstAccessor<float> rTrkWire   {"rTrkWire"};
+    const SG::AuxElement::ConstAccessor<int>   type       {"type"};
+    const SG::AuxElement::ConstAccessor<float> localX     {"localX"};
+    const SG::AuxElement::ConstAccessor<float> localY     {"localY"};
+    const SG::AuxElement::ConstAccessor<float> localTheta {"localTheta"};
+    const SG::AuxElement::ConstAccessor<float> localPhi   {"localPhi"};
+    const SG::AuxElement::ConstAccessor<float> HitZ       {"HitZ"};
+    const SG::AuxElement::ConstAccessor<float> HitR       {"HitR"};
+    const SG::AuxElement::ConstAccessor<float> rTrkWire   {"rTrkWire"};
 
   }
 }
